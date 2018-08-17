c8f18b55a34d1a55da395d88c7606417ebf0479c
skybearr <40344680@qq.com>
2018-08-17 18:12:21 +0800

    1

--- lifeGame/libs/exml.e.d.ts
+++ lifeGame/libs/exml.e.d.ts
@@ -2,6 +2,10 @@ declare class BaseButtonSkin extends eui.Skin{
 }
 declare class GameSkin extends eui.Skin{
 }
+declare class MainBtn extends eui.Skin{
+}
+declare class MainBtnSend extends eui.Skin{
+}
 declare class MarketItemSkin extends eui.Skin{
 }
 declare class RankSkin extends eui.Skin{
